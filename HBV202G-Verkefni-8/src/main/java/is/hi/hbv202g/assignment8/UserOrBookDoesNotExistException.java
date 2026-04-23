dfc1b9611d5d6cc72e9dc9dec84cbe6d108f7501
Aurawra <auroralaufey12@gmail.com>
2026-04-23 13:54:52 +0000

    A

--- HBV202G-Verkefni-8/src/main/java/is/hi/hbv202g/assignment8/UserOrBookDoesNotExistException.java
+++
@@ -1,8 +0,0 @@
-package is.hi.hbv202g.assignment8;
-
-public class UserOrBookDoesNotExistException extends Exception {
-    public UserOrBookDoesNotExistException(String message) {
-        super(message);
-    }
-
-}
